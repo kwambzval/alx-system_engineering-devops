6ac2557ade4f3403ac59ee978dd4c6cf1bd9702a
kwambzval <valentinevkkv@gmail.com>
2024-07-29 13:37:49 -0700

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+
+    # Fetch user data
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    # Fetch todos data
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
+
